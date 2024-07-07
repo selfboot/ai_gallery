d313fd457c11873c8c90316cc076c37226b7e27d
selfboot <xuezaigds@gmail.com>
2024-07-07 15:24:40 +0800

    Fix warning

--- src/language_switcher.js
+++ src/language_switcher.js
@@ -16,7 +16,7 @@ const LanguageSwitcher = () => {
     }
 
     setLanguage(i18n.language);
-  }, [i18n.language]);
+  }, [i18n]);
 
   const handleLanguageChange = (event) => {
     const selectedLang = event.target.value;
