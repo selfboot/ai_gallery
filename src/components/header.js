3f61aee6f993394d6823b3b158b0d6610da9526f
selfboot <xuezaigds@gmail.com>
2024-07-12 19:58:59 +0800

    Fix build fail because use window in SSR

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faCode } from "@fortawesome/free-solid-svg-icons";
 import { useLocation } from "@reach/router";
@@ -8,8 +8,9 @@ import { navigate } from "gatsby";
 const PageHeader = () => {
   const location = useLocation();
   const { t } = useTranslation();
-  const getTitle = () => {
+  const [codeLink, setCodeLink] = useState("");
 
+  const getTitle = () => {
     const path = location.pathname.split("/")[2];
     if (path) return t(path + "_title");
     return t("home");
@@ -18,26 +19,29 @@ const PageHeader = () => {
   const handleBack = () => {
     const url = new URL(window.location);
     const pathSegments = url.pathname.split("/").filter(Boolean);
-    pathSegments.pop(); // 移除最后一部分
+    pathSegments.pop();
     const parentPath = `/${pathSegments.join("/")}`;
     navigate(parentPath ? parentPath : "/", { replace: true });
   };
 
-  const getCodeLink = () => {
-    const url = new URL(window.location);
-    const pathSegments = url.pathname.split("/").filter(Boolean);
-    const filePath = pathSegments.join("/");
-    const baseUrl = "https://github.com/selfboot/ai_gallery/tree/main/src/pages";
-    return `${baseUrl}/${filePath}.js`;
-  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const url = new URL(window.location);
+      const pathSegments = url.pathname.split("/").filter(Boolean);
+      const filePath = pathSegments.join("/");
+      const baseUrl = "https://github.com/selfboot/ai_gallery/tree/main/src/pages";
+      setCodeLink(`${baseUrl}/${filePath}.js`);
+    }
+  }, [location]);
 
-  const codeLink = getCodeLink();
   return (
     <div className="flex items-center p-4">
       <h1 className="text-xl"> {getTitle()} </h1>
-      <a href={codeLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 cursor-pointer flex items-center ml-4">
-        <FontAwesomeIcon icon={faCode} className="mr-2" />
-      </a>
+      {codeLink && (
+        <a href={codeLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 cursor-pointer flex items-center ml-4">
+          <FontAwesomeIcon icon={faCode} className="mr-2" />
+        </a>
+      )}
       <span onClick={handleBack} className="text-blue-500 hover:text-blue-700 cursor-pointer flex items-center ml-4">
         <FontAwesomeIcon icon={faArrowLeft} />
       </span>
