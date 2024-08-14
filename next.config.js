5dcc3cb582ae6838bd048e081430f53159d23825
selfboot <xuezaigds@gmail.com>
2024-08-14 14:42:30 +0800

    Revert "Fix ContentSecurityPolicy"
    This reverts commit 047737a9cf51ed366e45026c0244f3d813218cad.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const path = require('path');
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://giscus.app;
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' blob: data: https://slefboot-1251736664.file.myqcloud.com;
-  frame-src https://giscus.app;
-  connect-src 'self' https://giscus.app;
-`;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -26,19 +17,6 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-          }
-        ]
-      }
-    ];
   },
 };
 
