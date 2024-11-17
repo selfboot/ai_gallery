97d14e7ffc1e0b9d4eea405579e9fc227d0b1e7f
selfboot <xuezaigds@gmail.com>
2024-11-17 09:24:44 +0800

    Sokoban: fix

--- src/app/[lang]/games/sokoban/page.js
+++ src/app/[lang]/games/sokoban/page.js
@@ -13,8 +13,8 @@ export async function generateMetadata({ params: { lang } }) {
     description: dict.seo.sokoban.description,
     keywords: dict.seo.sokoban.keywords,
     canonicalUrl: `https://gallery.selfboot.cn/${lang}/games/sokoban`,
-    publishedDate: "2024-11-13T02:00:00.000Z",
-    updatedDate: "2024-11-18T06:00:00.000Z",
+    publishedDate: "2024-11-17T03:00:00.000Z",
+    updatedDate: "2024-11-17T03:00:00.000Z",
   });
 }
 
