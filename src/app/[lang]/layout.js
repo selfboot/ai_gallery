5020e2f9d21da111a954deb27c38084a36f83d30
selfboot <xuezaigds@gmail.com>
2024-12-19 10:38:25 +0800

    Layout: add Microsoft Clarity

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -36,6 +36,19 @@ export default async function Layout({ children, params: { lang, slug = [] } }) 
           async
           crossOrigin="anonymous"
         />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "hvful8k59h");
+            `,
+          }}
+        />
       </head>
       <body>
         <div className="flex flex-col min-h-screen">
