c4d73d8283f10c462594f73c137a45d7a2c84e4c
selfboot <xuezaigds@gmail.com>
2024-12-25 13:16:07 +0800

    hanoitower: improve hanoti

--- src/app/components/AdComponent.js
+++ src/app/components/AdComponent.js
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
-// 共用的广告初始化逻辑
+// Common ad init logic
 function useAdInit() {
   const isLoaded = useRef(false);
 
@@ -15,37 +15,34 @@ function useAdInit() {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
       isLoaded.current = true;
     } catch (err) {
-      console.error('Ad error:', err);
+      console.error("Ad error:", err);
     }
   }, []);
 }
 
 // https://support.google.com/adsense/answer/9183460?hl=zh-Hans&sjid=15277587185637410503-AP
-// 基础广告组件
-function BaseAdComponent({ slot, className }) {
+// format:  “rectangle”、“vertical”、“horizontal”
+function BaseAdComponent({ slot, className, format = "rectangle" }) {
   useAdInit();
 
   return (
-    <div className={`relative ${className || ''}`}>
+    <div className={`relative ${className || ""}`}>
       <ins
         className="adsbygoogle"
-        style={{ display: 'block' }}
+        style={{ display: "block" }}
         data-ad-client="ca-pub-7746897490519544"
         data-ad-slot={slot}
-        data-ad-format="rectangle"
+        data-ad-format={format}
         data-full-width-responsive="false"
       />
     </div>
   );
 }
 
-export function AdComponent() {
-  return <BaseAdComponent slot="4833706118" />;
+export function SideAdComponent({ className, format }) {
+  return <BaseAdComponent slot="7437487022" format={format} className={`overflow-hidden ${className || ""}`} />;
 }
 
-export function SideAdComponent({ className }) {
-  return <BaseAdComponent 
-    slot="7437487022" 
-    className={`overflow-hidden ${className || ''}`}
-  />;
+export function AdComponent({ format }) {
+  return <BaseAdComponent slot="4833706118" format={format} />;
 }
