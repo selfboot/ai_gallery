f19aae74230d5efc8160dac40850398514354c33
selfboot <xuezaigds@gmail.com>
2025-01-13 13:57:44 +0800

    Tool award: add more award template tool

--- src/app/[lang]/tools/gendocx/templates.js
+++ src/app/[lang]/tools/gendocx/templates.js
@@ -18,6 +18,15 @@ const documentTemplates = [
     updatedDate: "2025-01-08T09:00:00.000Z",
   },
   {
+    id: 'award_3',
+    previewImage: 'https://slefboot-1251736664.file.myqcloud.com/20250113_gendocx_award_3.png',
+    path: 'https://slefboot-1251736664.file.myqcloud.com/award_3.docx',
+    previewImageAlt: 'award_alt',
+    excelTemplatePath: 'https://slefboot-1251736664.file.myqcloud.com/award_3.xlsx',
+    publishedDate: "2025-01-13T09:00:00.000Z",
+    updatedDate: "2025-01-13T09:00:00.000Z",
+  },
+  {
     id: 'certificate_1',
     previewImage: 'https://slefboot-1251736664.file.myqcloud.com/20250108_gendocx_certificate_1.png',
     path: 'https://slefboot-1251736664.file.myqcloud.com/certificate_1.docx',
